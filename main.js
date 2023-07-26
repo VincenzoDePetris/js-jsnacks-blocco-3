6560959780f4af4f233bcceffd255b019054b872
VincenzoDePetris <vince02@live.it>
2023-07-26 15:39:45 +0200

    add: condition

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const primoNumero = parseInt(prompt("scrivi un numero"));
 const secondoNumero = parseInt(prompt("scrivi un altro numero"));
 
+if (secondoNumero > primoNumero) {
+  var random = Math.floor(
+    Math.random() * (secondoNumero - primoNumero + 1) + primoNumero
+  );
+}
 var random = Math.floor(
-  Math.random() * (secondoNumero - primoNumero + 1) + primoNumero
+  Math.random() * (primoNumero - secondoNumero + 1) + secondoNumero
 );
 
 console.log(random);
