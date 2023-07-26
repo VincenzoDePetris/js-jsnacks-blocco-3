69e773591bd8ccfe0a877d7b66214c3899725135
VincenzoDePetris <vince02@live.it>
2023-07-26 16:36:37 +0200

    add: terza traccia

--- main.js
+++ main.js
@@ -12,13 +12,22 @@
 
 // console.log(random);
 
-const primaParola = prompt("inserisci una parola");
-const secondaParola = prompt("inserisci un'altra parola");
+// const primaParola = prompt("inserisci una parola");
+// const secondaParola = prompt("inserisci un'altra parola");
 
-if (primaParola.length > secondaParola.length) {
-  console.log(primaParola);
-} else if (secondaParola.length > primaParola.length) {
-  console.log(secondaParola);
-} else {
-  console.log(primaParola + " " + secondaParola);
+// if (primaParola.length > secondaParola.length) {
+//   console.log(primaParola);
+// } else if (secondaParola.length > primaParola.length) {
+//   console.log(secondaParola);
+// } else {
+//   console.log(primaParola + " " + secondaParola);
+// }
+
+const arr = [];
+let sum = 0;
+
+while (sum < 50) {
+  const numero = parseInt(prompt("scrivi un numero"));
+  sum += numero;
+  arr.push(numero);
 }
--- readme.md
+++ readme.md
@@ -19,3 +19,13 @@ Dare la possibilità di inserire due parole. Verificare che le due parole abbian
 - verifico quale è la più lunga
   - o stampo la più lunga
   - se sono uguali le stampo entrambe
+
+# TERZA TRACCIA
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+## svolgimento
+
+- creo un array vuoto
+- chiedo all'utente un numero
+- ripeto la cosa finchè la somma dell'array è minore di 50
