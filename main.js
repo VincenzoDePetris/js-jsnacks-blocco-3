84f1294bfb64dcbf447479aeb63c130dba0b9a98
VincenzoDePetris <vince02@live.it>
2023-07-26 15:35:54 +0200

    reload: espressione

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const primoNumero = parseInt(prompt("scrivi un numero"));
 const secondoNumero = parseInt(prompt("scrivi un altro numero"));
 
-let random = Math.floor(Math.random() * secondoNumero + primoNumero);
+var random = Math.floor(
+  Math.random() * (secondoNumero - primoNumero + 1) + primoNumero
+);
 
 console.log(random);
