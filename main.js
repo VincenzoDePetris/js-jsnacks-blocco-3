8cb88afd3948853157c30fd88477359c1587343a
VincenzoDePetris <vince02@live.it>
2023-07-26 17:25:47 +0200

    fix: ";"

--- main.js
+++ main.js
@@ -47,8 +47,7 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 let evenNumbers = [];
 for (let i = 0; i < numbers.length - 1; i++) {
-  if (numbers[i] % 2 == 0);
-  {
+  if (numbers[i] % 2 == 0) {
     evenNumbers.push(i);
   }
 }
